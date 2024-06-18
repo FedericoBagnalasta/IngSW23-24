15cb683b1cc29bd25a72243f812da76cd3d47bad
matteokovacic <matteokovacic@localhost>
2024-06-18 13:41:50 +0200

    risoluzione problemi classe Login

--- IngegneriaDelSW23_24/src/it/unibs/controller/Login.java
+++ IngegneriaDelSW23_24/src/it/unibs/controller/Login.java
@@ -11,16 +11,16 @@ public class Login {
 		else return false;
 	}
 	
-	public void cambiaCredenziali() {
+	public void cambiaCredenziali(String nome) {
 		do {
 			//Si richiama un metodo della view che richiede all'utente di inserire le nuove credenziali
-		} while(!verificaUnicita(String nome));
+		} while(!verificaUnicita(nome));
 		//Crea un oggetto credenziali e lo aggiunge al loro elenco
 	}
 	
-	public void login() {
+	public void login(String nome, String password) {
 		//La view restituisce il nome e la password (inseriti dall'utente) che si utilizzano di seguito
-		if(verificaPrimoAccesso(String nome, String password)) {
+		if(verificaPrimoAccesso(nome, password)) {
 			cambiaCredenziali();
 		}
 		
@@ -29,5 +29,7 @@ public class Login {
 	public boolean verificaUnicita(String nome) {
 		//Serve un ciclo che controlla il nome di tutte le persone (conf e fruit) che saranno salvati su un arraylist
 		//Oppure facciamo un hashmap di tutte le credenziali e in questo caso guardiamo solo il nome (chiave)
+		
+		return false;
 	}
 }
