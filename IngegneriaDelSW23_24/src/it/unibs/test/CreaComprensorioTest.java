84666df7254b259cbab0a67ac0167c4992d66379
Federico Bagnalasta <federico.bagnalasta@gmail.com>
2024-08-19 17:01:12 +0200

    test black box

--- IngegneriaDelSW23_24/src/it/unibs/test/CreaComprensorioTest.java
+++ IngegneriaDelSW23_24/src/it/unibs/test/CreaComprensorioTest.java
@@ -17,21 +17,38 @@ class CreaComprensorioTest {
 
 	@Test
 	void creaComprensorioTest() {
-		String simulatedInput = "comprensorio\ncomune1\ns\ncomune2\nn\n";
+		String simulatedInput = "comprensorio\ncomune1\ns\ncomune2\nn\n" + // (1), (4)
+				"comprensorio\ncomprensorio2\ncomune1\ns\ncomune1\ncomune3\nn\n" + // (2), (5)
+				"\ncomprensorio3\n\ncomune1\nn\n"; // (3), (6)
 		ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
 		System.setIn(inputStream);
-		
+
 		comprensorioController.creaComprensorio();
-		
+		comprensorioController.creaComprensorio();
+		comprensorioController.creaComprensorio();
+
 		ArrayList<Comprensorio> elencoComprensori = ElencoComprensori.getElencoComprensori();
-		
+
 		ArrayList<String> comuni = new ArrayList<>();
 		comuni.add("comune1");
 		comuni.add("comune2");
-		
-		assertEquals(elencoComprensori.get(0).getNome(), "comprensorio");
-		assertEquals(elencoComprensori.get(0).getComuniComprensorio(), comuni);
-		
+
+		ArrayList<String> comuni2 = new ArrayList<>();
+		comuni2.add("comune1");
+		comuni2.add("comune3");
+
+		ArrayList<String> comuni3 = new ArrayList<>();
+		comuni3.add("comune1");
+
+		assertEquals(elencoComprensori.get(0).getNome(), "comprensorio");// (1)
+		assertEquals(elencoComprensori.get(0).getComuniComprensorio(), comuni);// (4)
+
+		assertEquals(elencoComprensori.get(1).getNome(), "comprensorio2");// (2)
+		assertEquals(elencoComprensori.get(1).getComuniComprensorio(), comuni2);// (5)
+
+		assertEquals(elencoComprensori.get(2).getNome(), "comprensorio3");// (3)
+		assertEquals(elencoComprensori.get(2).getComuniComprensorio(), comuni3);// (6)
+
 		System.setIn(System.in);
 	}
 }
